efd83086e28b4adb55b97540c9542d43ace8cb63
Matthew Pumeshin Moodley <matthew@Matthews-MacBook-Air.local>
2025-11-24 21:18:27 +0200

    feat(main): added running command line

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 import (
-	"fmt";
-  "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
+	fmt.Print("Pokedex > ")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	map[string] cliCommand {
+    	"exit": {
+        	name:        "exit",
+      	  description: "Exit the Pokedex",
+    	    callback:    commandExit,
+  	  },
+	}
+
+	for scanner.Scan() {
+		if cliCommand[scanner.Text()] != nil{
+			return cliCommand[scanner.Text()].callback
+		}
+		fmt.Print("Pokedex > ")
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+
 }
+
 
 func cleanInput(text string) []string {
   slice := []string{}
@@ -15,3 +40,18 @@ func cleanInput(text string) []string {
 	slice = strings.Fields(strings.ToLower(text))
 	return slice
 }
+
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+
